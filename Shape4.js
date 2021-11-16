5561aa8f75020c503d9f2648da23a7b64d9ec698
MahreenAthar <mahreenathar.oct10@gmail.com>
2021-11-17 01:46:28 +0500

    Body, Leg, and Head done.

---
+++ Shape4.js
@@ -0,0 +1,29 @@
+var w = 680;
+var h = 680;
+var svg = d3.select("body").append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+// Head Male
+var headM = svg.append("circle")
+               .attr("r", "40")
+               .attr("cx", "160")
+               .attr("cy", "60")
+               .attr("fill", "black")
+
+// Body Male
+var bodyM = svg.append("rect")
+               .attr("height", "200")
+               .attr("width", "80")
+               .attr("x", "120")
+               .attr("y", "120")
+               .attr("rx", "10")
+
+// Legs Male
+var leg1M = svg.append("rect")
+               .attr("height", "100")
+               .attr("width", "25")
+               .attr("x", "120")
+               .attr("y", "300")
+               .attr("rx", "10")
+               .attr("ry", "10")
