2eae591c021889dda8c37abc654e1be61422674b
MahreenAthar <mahreenathar.oct10@gmail.com>
2021-11-17 01:56:51 +0500

    Legs done.

--- Shape4.js
+++ Shape4.js
@@ -20,10 +20,41 @@ var bodyM = svg.append("rect")
                .attr("rx", "10")
 
 // Legs Male
-var leg1M = svg.append("rect")
+var legR1M = svg.append("rect")
                .attr("height", "100")
                .attr("width", "25")
                .attr("x", "120")
                .attr("y", "300")
                .attr("rx", "10")
                .attr("ry", "10")
+var legBentR1M = svg.append("rect")
+               .attr("height", "100")
+               .attr("width", "25")
+               .attr("x", "340")
+               .attr("y", "207")
+               .attr("rx", "10")
+               .attr("ry", "10")
+               .attr("transform", "rotate(40)")
+var legL1M = svg.append("rect")
+               .attr("height", "200")
+               .attr("width", "25")
+               .attr("x", "175")
+               .attr("y", "300")
+               .attr("rx", "10")
+               .attr("ry", "10")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
