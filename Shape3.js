95ad8cd82694f0a1aeb5d4585d51d1efe82fae1c
MahreenAthar <mahreenathar.oct10@gmail.com>
2021-11-17 01:17:32 +0500

    Legs added

--- Shape3.js
+++ Shape3.js
@@ -20,3 +20,91 @@ svg.append('circle')
     .attr('fill', 'white')  
     .attr('stroke', 'black')
     .attr('stroke-width',6)
+
+// Body
+svg.append('circle')
+    .attr('cx', 255)
+    .attr('cy', 205)
+    .attr('r', 35)
+    .attr('fill', 'black')  
+    .attr('stroke', 'black')
+
+  
+    svg.append("rect")
+    .attr("x",255)
+    .attr("y",170)
+    .attr('width',180)
+    .attr('height',71)
+    
+  
+// Arm
+svg.append("rect")
+    .attr('width',140)
+    .attr('height',35)
+    .attr('rx',15)
+    .attr('ry',15)
+    .attr('transform', 'translate(250, 200) rotate(49)')
+   
+svg.append("rect")
+    .attr('width',35)
+    .attr('height',140)
+    .attr('rx',15)
+    .attr('ry',15)
+    .attr('transform', 'translate(305, 290) rotate(43)')
+  
+// Leg
+svg.append("rect")
+    .attr("x",448)
+    .attr("y",190)
+    .attr('width',35)
+    .attr('height',270)
+    
+    
+svg.append('circle')
+    .attr('cx', 465)
+    .attr('cy', 465)
+    .attr('r', 18)
+    .attr('fill', 'black')  
+    .attr('stroke', 'black')
+    
+svg.append("rect")
+    .attr('width',25)
+    .attr('height',190)
+    .attr('rx',15)
+    .attr('ry',15)
+    .attr('transform', 'translate(3550, 60) rotate(43)')
+    
+svg.append("rect")
+    .attr('width',30)
+    .attr('height',200)
+    .attr('transform', 'translate(470, 190) rotate(43)')
+    
+svg.append("rect")
+    .attr('width',55)
+    .attr('height',187)
+     .attr('fill', 'white')  
+    .attr('stroke', 'white')
+    .attr('transform', 'translate(480, 110) rotate(46)')
+    
+svg.append("rect")
+    .attr('width',200)
+    .attr('height',30)
+    .attr('rx',15)
+    .attr('ry',15)
+    .attr('transform', 'translate(345, 325) rotate(46)')
+        
+svg.append("rect")
+    .attr("x",448)
+    .attr("y",360)
+    .attr('rx',15)
+    .attr('ry',15)
+    .attr('width',35)
+    .attr('height',170)
+    
+    svg.append("rect")
+    .attr("x",484)
+    .attr("y",200)
+    .attr('width',15)
+    .attr('height',30)
+     .attr('fill', 'white')  
+    .attr('stroke', 'white')
